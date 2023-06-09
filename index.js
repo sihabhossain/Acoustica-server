eb43011b1dfe99d9616f2d30736abcb187b14c5a
Sihab Hossain <sihabhossain380@gmail.com>
2023-06-10 02:03:36 +0600

    admin update api done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jaehzkc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,6 +53,19 @@ async function run() {
         // get all users
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        // update user to admin
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc)
             res.send(result)
         })
 
